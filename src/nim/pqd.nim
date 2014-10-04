061f5b899b9a6ee1a876dcee1c9c82e0c6d1a34e
Torbjørn Marø <torbjorn.maro@gmail.com>
2014-10-04 23:45:36 +0200

    Made thread safe with locks

--- src/nim/pqd.nim
+++ src/nim/pqd.nim
@@ -1,4 +1,4 @@
-import jester, json, tables
+import jester, json, tables, locks
 
 type
   PNode = ref TNode
@@ -21,29 +21,37 @@ proc newQueue(key: string): PQueue =
   result.queue = key
   result.count = 0
 
-var queues = initTable[string, PQueue]()
+var 
+  queues = initTable[string, PQueue]()
+  L: TLock
 
 proc getQueue(key: string): PQueue =
+  acquire(L)
   if not queues.hasKey(key):
     queues[key] = newQueue(key)
+  release(L)
   result = queues[key] 
 
 proc push(q: PQueue, val: string) =
-  var node = newNode(val, previous = q.last)
+  acquire(L)
+  let node = newNode(val, previous = q.last)
   if q.count == 0:
     q.first = node
   else:
     node.prev.next = node
   q.last = node
   q.count = q.count + 1
+  release(L)
   
 proc pop(q: PQueue): PNode =
+  acquire(L)
   result = q.first
   if result != nil:
     q.first = result.next
     q.count = q.count - 1
     if q.first != nil:
       q.first.prev = nil
+  release(L)
 
 proc `%`(q: PQueue): PJsonNode =
   result = %[("queue", %q.queue), ("count", %q.count)]
@@ -68,5 +76,7 @@ get "/@queue":
 get "/@queue/count":
   let queue = getQueue(@"queue")
   resp($ %queue, "application/json")
+
+initLock(L)
 
 run()
